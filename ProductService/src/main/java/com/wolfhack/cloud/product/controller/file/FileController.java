e16813141abc5c43dccb31eb48e853e074aa0d5d
iNikitaGricenko <VfVristalt@gmail.com>
2023-07-04 18:34:35 +0300

    add endpoints for upload files

--- ProductService/src/main/java/com/wolfhack/cloud/product/controller/file/FileController.java
+++ ProductService/src/main/java/com/wolfhack/cloud/product/controller/file/FileController.java
@@ -6,25 +6,36 @@ import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/file")
 @RequiredArgsConstructor
 public class FileController {
 
 	private final AmazonStorageServiceInterface storageService;
 
 	@RequestMapping(
-			value = "/downloads/{fileId}",
+			value = "/download/{fileId}",
 			method = {RequestMethod.GET, RequestMethod.OPTIONS},
 			produces = {MediaType.APPLICATION_OCTET_STREAM_VALUE})
 	@ResponseStatus(value = HttpStatus.OK)
 	public @ResponseBody byte[] downloadFile(@PathVariable("fileId") String fileId) throws IOException {
 		InputStream inputStream = storageService.download(fileId);
 		return IOUtils.toByteArray(inputStream);
+	}
+
+	@RequestMapping(
+			value = "/upload",
+			method = RequestMethod.POST,
+			consumes = MediaType.APPLICATION_OCTET_STREAM_VALUE
+	)
+	@ResponseStatus(value = HttpStatus.OK)
+	public void uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+		storageService.upload(file);
 	}
 
 }
